611bb77be9756464b1a3ade6f296fec1d13c343b
Sharat M R <cosmosgenius@gmail.com>
2013-09-19 03:06:27 +0530

    finished loop function initialization left

--- basegl.cpp
+++ basegl.cpp
@@ -1,21 +1,103 @@
 #include "basegl.h"
+#include <QtCore/QCoreApplication>
 
-basegl::basegl(QWindow *parent):QWindow(parent)
+basegl::basegl(QWindow *parent)
+    :QWindow(parent)
+    ,m_update_pending(false)
+    ,m_animating(false)
+    ,m_context(0)
+    ,m_funcs(0)
+    ,max_fps(66)
 {
-    m_animate = false;
+    setSurfaceType(QWindow::OpenGLSurface);
+    m_context = new QOpenGLContext(this);
+    m_context->create();
+    m_context->makeCurrent(this);
+
+    if(needsInitialize){
+        m_funcs = m_context->versionFunctions<QOpenGLFunctions_3_1>();
+        if(!m_funcs){
+            qDebug() << "m_funcs initialize failed";
+            exit(1);
+        }
+        m_funcs->initializeOpenGLFunctions();
+        initialize();
+    }
+}
+
+void basegl::initialize(){
+
+}
+
+void basegl::render(QPainter *painter){
+    Q_UNUSED(painter);
 }
 
 void basegl::render(){
+    if (!m_device)
+        m_device = new QOpenGLPaintDevice;
 
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+    m_device->setSize(size());
+
+    QPainter painter(m_device);
+    render(&painter);
 }
 
-void basegl::startAnimation(){
-    if(m_animate)
+void basegl::setAnimation(bool animating){
+    m_animating = animating;
+    if(m_animating)
+        renderLater();
+}
+
+void basegl::renderNow(){
+    if(!isExposed())
         return;
-    m_animate = true;
-    renderLater();
+
+    bool needsInitialize = false;
+
+    m_context->makeCurrent(this);
+
+
+
+    render();
+
+    m_context->swapBuffers(this);
+
+    if(m_animating)
+        renderLater();
 }
 
-void basegl::stopAnimation(){
-    m_animate = false;
+void basegl::renderLater(){
+    if (!m_update_pending) {
+        m_update_pending = true;
+        QCoreApplication::postEvent(this, new QEvent(QEvent::UpdateRequest));
+    }
+}
+void basegl::event(QEvent *event){
+    switch (event->type()) {
+    case QEvent::UpdateRequest:
+        m_update_pending = false;
+        renderNow();
+        return true;
+    default:
+        return QWindow::event(event);
+    }
+}
+
+void basegl::exposeEvent(QExposeEvent *event){
+    Q_UNUSED(event);
+
+    if(isExposed())
+        renderLater();
+}
+
+void basegl::getMax_fps(){
+    return max_fps;
+}
+
+void basegl::setMax_fps(int fps){
+    if(fps > 0)
+        max_fps = fps;
 }
--- basegl.h
+++ basegl.h
@@ -7,17 +7,34 @@
 class basegl : public QWindow
 {
     Q_OBJECT
+
+public slots:
+    void renderLater();
+    void renderNow();
+
 public:
     explicit basegl(QWindow *parent = 0);
+    ~basegl();
 
+    virtual void render(QPainter *painter);
     virtual void render();
+
     virtual void initialize();
 
-    void startAnimation();
-    void stopAnimation();
+    void setAnimation();
 
 private:
-    bool m_animate;
+    bool m_update_pending;
+    bool m_animating;
+    int max_fps;
+
+    QOpenGLContext *m_context;
+    QOpenGLPaintDevice *m_device;
+    QOpenGLFunctions_3_1 *m_funcs;
+
+protected:
+    void exposeEvent(QExposeEvent *);
+    void event(QEvent *);
 };
 
 #endif // BASEGL_H
