0acf028d6bda539c3348066c964e7a11fae62c12
Sharat M R <cosmosgenius@gmail.com>
2013-10-03 01:26:43 +0530

    added two triangles and showing them alternatively

--- openglwindow.cpp
+++ openglwindow.cpp
@@ -7,7 +7,7 @@ OpenGLWindow::OpenGLWindow(QWindow *parent):Basegl(parent)
     m_funcs = getFunc();
     const GLubyte *ver = m_funcs->glGetString(GL_VERSION);
     qDebug() << (char*) ver;
-
+    increment = 0;
 }
 
 OpenGLWindow::~OpenGLWindow()
@@ -17,18 +17,32 @@ OpenGLWindow::~OpenGLWindow()
 
 void OpenGLWindow::render()
 {
+
     static const GLfloat g_vertex_buffer_data[] = {
-        -1.0f, -1.0f, 0.0f,
-        1.0f, -1.0f, 0.0f,
-        0.0f, 1.0f, 0.0f,
+       -0.1f, -0.1f, 0.0f,
+        0.1f, -0.1f, 0.0f,
+        0.0f, 0.1f, 0.0f,
     };
+
+    static const GLfloat g_vertex_buffer_data1[] = {
+        -0.4f, -0.4f, 0.0f,
+        0.4f, -0.4f, 0.0f,
+        0.0f, 0.4f, 0.0f,
+    };
+
     GLuint vertexbuffer;
     m_funcs->glClearColor(0.0f,0.0f,0.0f,0.0f);
     m_funcs->glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
     m_funcs->glGenBuffers(1,&vertexbuffer);
     m_funcs->glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    m_funcs->glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data,GL_STATIC_DRAW);
-
+    if(increment < 50){
+        m_funcs->glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data,GL_STATIC_DRAW);
+    }else if(increment < 100){
+        m_funcs->glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data1), g_vertex_buffer_data1,GL_STATIC_DRAW);
+    }else{
+        m_funcs->glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data1), g_vertex_buffer_data1,GL_STATIC_DRAW);
+        increment = 0;
+    }
     m_funcs->glEnableVertexAttribArray(0);
     m_funcs->glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     m_funcs->glVertexAttribPointer(
@@ -41,6 +55,7 @@ void OpenGLWindow::render()
                 );
     m_funcs->glDrawArrays(GL_TRIANGLES, 0, 3);
     m_funcs->glDisableVertexAttribArray(0);
+    increment++;
     //qDebug() << "Render called";
 }
 
